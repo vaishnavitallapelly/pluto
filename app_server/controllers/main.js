8776eb46f20d9fa050155f55514dd9835db21b36
unknown <manohari3299550@gmail.com>
2024-07-13 10:29:12 -0700

    first commit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET homepage */
 const index = (req, res) => {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'online voting system' });
     };
     module.exports = {
         index
